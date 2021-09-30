1accacd32335c3d2634daff6782d94b2decfac40
emacser <roxman98@naver.com>
2021-09-30 10:51:00 +0900

    Added Singleton design pattern for RenderingContext

---
+++ RenderingContext.py
@@ -0,0 +1,11 @@
+import pico2d
+
+from Singleton import *
+
+class RenderingContext(Singleton):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        pico2d.open_canvas()
+        pico2d.resize_canvas(self.width, self.height)
