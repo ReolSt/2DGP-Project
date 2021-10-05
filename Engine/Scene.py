273967598a84af2bd1f77d3469129e1069d9340e
emacser <roxman98@naver.com>
2021-10-05 21:52:17 +0900

    Update scene update mechanism

--- Engine/Scene.py
+++ Engine/Scene.py
@@ -1,7 +1,13 @@
 import pico2d
 from .GameObject import *
+from .CollisionManager import *
 
 class Scene:
     def __init__(self, name=""):
         self.name = name
         self.root = GameObject(None)
+        self.collisionManager = CollisionManager(self)
+
+    def Update(self, deltaTime):
+        self.root.Update(deltaTime)
+        self.collisionManager.Update()
