e1a78f99051b8b1210b3053ef949cd5d0304dac1
emacser <roxman98@naver.com>
2021-10-06 13:42:15 +0900

    Delete useless custom object class

--- Engine/TerrainObject.py
+++
@@ -1,9 +0,0 @@
-import pico2d
-
-from .TerrainSprite import *
-from .GameObject import *
-
-class TerrainObject(GameObject):
-    def __init__(self, parent, spriteName):
-        super().__init__(parent)
-        self.sprites.append(TerrainSprite(self.transform, spriteName))
