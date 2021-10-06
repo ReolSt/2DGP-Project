bc91fc0c7383699d0c2a586e879a1f252d0c19a5
emacser <roxman98@naver.com>
2021-10-07 00:04:04 +0900

    Update Cloud.py

--- Cloud.py
+++ Cloud.py
@@ -2,28 +2,46 @@ from Engine.GameObject import *
 from Engine.TerrainSprite import *
 
 class Cloud(GameObject):
-    def __init__(self, parent):
+    def __init__(self, parent, width=2):
+        assert width >= 2, "Impossible Cloud width : {}".format(width)
+
         super().__init__(parent)
 
         bottomLeft = TerrainSprite(self.transform, "CloudBottom1")
-        bottomCenter = TerrainSprite(self.transform, "CloudBottom2")
-        bottomRight = TerrainSprite(self.transform, "CloudBottom3")
-
-        bottomLeft.transform.translate(-bottomLeft.width, 0)
-        bottomRight.transform.translate(bottomRight.width, 0)
-
         topLeft = TerrainSprite(self.transform, "CloudTop1")
-        topCenter = TerrainSprite(self.transform, "CloudTop2")
-        topRight = TerrainSprite(self.transform, "CloudTop3")
 
-        topLeft.transform.translate(-topLeft.width, topLeft.height)
-        topCenter.transform.translate(0, topCenter.height)
-        topRight.transform.translate(topRight.width, topLeft.height)
+        referenceSprite = bottomLeft
+        spriteWidth = referenceSprite.width
+        spriteHeight = referenceSprite.height
+
+        xOffset = spriteWidth / 2
+        yOffset = spriteHeight / 2
+
+        bottomLeft.transform.translate(xOffset, yOffset)
+        topLeft.transform.translate(xOffset, yOffset + spriteHeight)
+
+        xOffset += spriteWidth
 
         self.sprites.append(bottomLeft)
-        self.sprites.append(bottomCenter)
-        self.sprites.append(bottomRight)
-
         self.sprites.append(topLeft)
-        self.sprites.append(topCenter)
+
+        for x in range(width - 2):
+            bottom = TerrainSprite(self, "CloudBottom2")
+            top = TerrainSprite(self, "CloudTop2")
+
+            bottom.transform.translate(xOffset, yOffset)
+            top.transform.translate(xOffset, yOffset + spriteHeight)
+
+            self.sprites.append(bottom)
+            self.sprites.append(top)
+
+            xOffset += spriteWidth
+
+        bottomRight = TerrainSprite(self.transform, "CloudBottom3")
+        topRight = TerrainSprite(self.transform, "CloudTop3")
+
+        bottomRight.transform.translate(xOffset, yOffset)
+        topRight.transform.translate(xOffset, yOffset + spriteHeight)
+
+        self.sprites.append(bottomRight)
         self.sprites.append(topRight)
