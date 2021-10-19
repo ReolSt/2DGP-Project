ef58910cdc0042a55adc0f97f83a5fa894ddbea9
emacser <roxman98@naver.com>
2021-10-20 04:04:11 +0900

    Add render layer to GamePlayInterface

--- GamePlayInterface.py
+++ GamePlayInterface.py
@@ -14,42 +14,50 @@ class GamePlayInterface(GameObject):
         self.name = Text(self, gameState.name, 1)
         self.name.transform.translate(80, 568)
         self.name.transform.setScale(3, 3)
+        self.name.layer = "UI"
         self.children.append(self.name)
 
         self.score = Text(self, str(gameState.score).zfill(6), 1)
         self.score.transform.translate(80, 544)
         self.score.transform.setScale(3, 3)
+        self.score.layer = "UI"
         self.children.append(self.score)
 
         self.coin = GameObject(self)
         self.coin.sprites.append(EntitySprite(self.coin, "Coin3"))
         self.coin.transform.translate(272, 544)
         self.coin.transform.setScale(2, 2)
+        self.coin.layer = "UI"
         self.children.append(self.coin)
 
         self.coinCount = Text(self, "*" + str(gameState.coin).zfill(2), 1)
         self.coinCount.transform.translate(298, 544)
         self.coinCount.transform.setScale(3, 3)
+        self.coinCount.layer = "UI"
         self.children.append(self.coinCount)
 
         self.world = Text(self, "WORLD", 1)
         self.world.transform.translate(464, 568)
         self.world.transform.setScale(3, 3)
+        self.world.layer = "UI"
         self.children.append(self.world)
 
         self.worldStage = Text(self, str(gameState.world) + "-" + str(gameState.stage), 1)
         self.worldStage.transform.translate(488, 544)
         self.worldStage.transform.setScale(3, 3)
+        self.worldStage.layer = "UI"
         self.children.append(self.worldStage)
 
         self.time = Text(self, "TIME", 1)
         self.time.transform.translate(640, 568)
         self.time.transform.setScale(3, 3)
+        self.time.layer = "UI"
         self.children.append(self.time)
 
         self.timeCount = Text(self, str(gameState.time).zfill(3), 1)
         self.timeCount.transform.translate(656, 544)
         self.timeCount.transform.setScale(3, 3)
+        self.timeCount.layer = "UI"
         self.children.append(self.timeCount)
 
     def update(self, deltaTime):
