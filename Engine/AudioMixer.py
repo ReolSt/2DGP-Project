c63a02bfdad51098f3fb5bf739ce2e41cfa811a1
emacser <roxman98@naver.com>
2021-10-20 05:18:32 +0900

    Add some features for AudioMixer and seperated Music and Wav

--- Engine/AudioMixer.py
+++ Engine/AudioMixer.py
@@ -7,10 +7,21 @@ class AudioMixer(metaclass=Singleton):
     def __init__(self):
 
         self.wavs = {}
+        self.musics = {}
 
         audioFilePath = Settings().audio["AudioFilePath"]
 
-        with open(Settings().audio["AudioFileListPath"], "r") as file:
+        with open(Settings().audio["MusicFileListPath"], "r") as file:
+            for line in file.readlines():
+                tokens = line.split()
+                if len(tokens) < 2:
+                    continue
+
+                name, fileName = tokens
+
+                self.musics[name] = pico2d.load_music(audioFilePath + fileName)
+
+        with open(Settings().audio["WavFileListPath"], "r") as file:
             for line in file.readlines():
                 tokens = line.split()
                 if len(tokens) < 2:
@@ -20,8 +31,53 @@ class AudioMixer(metaclass=Singleton):
 
                 self.wavs[name] = pico2d.load_wav(audioFilePath + fileName)
 
-    def play(self, name):
+    def getVolumeWav(self, name):
+        assert name in self.wavs, "Can't get wav volume : {}".format(name)
+
+        return self.wavs[name].get_volume()
+
+    def setVolumeWav(self, name, volume):
+        assert name in self.wavs, "Can't set wav volume : {}".format(name)
+
+        self.wavs[name].set_volume(volume)
+
+    def playWav(self, name):
         assert name in self.wavs, "Can't play wav : {}".format(name)
 
         self.wavs[name].play()
+
+    def getVolumeMusic(self, name):
+        assert name in self.wavs, "Can't get music volume : {}".format(name)
+
+        return self.musics[name].get_volume()
+
+    def setVolumeMusic(self, name, volume):
+        assert name in self.wavs, "Can't set music volume : {}".format(name)
+
+        self.musics[name].set_volume(volume)
+
+    def playMusic(self, name):
+        assert name in self.musics, "Can't play music : {}".format(name)
+
+        self.musics[name].play()
+
+    def playMusicRepeat(self, name):
+        assert name in self.musics, "Can't stop music : {}".format(name)
+
+        self.musics[name].repeat_play()
+
+    def stopMusic(self, name):
+        assert name in self.musics, "Can't stop music : {}".format(name)
+
+        self.musics[name].stop()
+
+    def pauseMusic(self, name):
+        assert name in self.musics, "Can't pause music : {}".format(name)
+
+        self.musics[name].pause()
+
+    def resumeMusic(self, name):
+        assert name in self.musics, "Can't resume music : {}".format(name)
+
+        self.musics[name].resume()
 
