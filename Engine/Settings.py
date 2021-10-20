7d6ab04d8467508b908bf91fa103a85e75f52f1e
emacser <roxman98@naver.com>
2021-10-21 04:29:30 +0900

    Update Settings.py

--- Engine/Settings.py
+++ Engine/Settings.py
@@ -15,3 +15,4 @@ class Settings(metaclass=Singleton):
         self.default = config['Default']
         self.sprite = config['Sprite']
         self.audio = config['Audio']
+        self.world = config['World']
