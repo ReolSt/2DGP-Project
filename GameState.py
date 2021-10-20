1f3737c210b91c2349a621e2ee5416c2527bcbac
emacser <roxman98@naver.com>
2021-10-21 04:29:45 +0900

    Make GameState Singleton

--- GameState.py
+++ GameState.py
@@ -1,11 +1,14 @@
+from Engine.Singleton import *
 from Engine.GameObject import *
 
-class GameState(GameObject):
-    def __init__(self, parent):
-        super().__init__(parent)
+class GameState(metaclass=Singleton):
+    def __init__(self):
         self.name = "MARIO"
         self.score = 0
         self.coin = 0
         self.world = 1
         self.stage = 1
         self.time = 0
+
+    def update(self, deltaTime):
+        self.time -= deltaTime / 1000
