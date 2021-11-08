977f95fc40d450f668873f47b850707941319d4b
Emacser <roxman98@naver.com>
2021-11-09 01:12:30 +0900

    [REFACTOR]: Refactor Transform class.

--- Engine/Transform.py
+++ Engine/Transform.py
@@ -14,34 +14,76 @@ class Transform:
         self.gameObject = gameObject
 
         self.localPosition = Vector2(0.0, 0.0)
-        self.localRotation = 0.0
         self.localScale = Vector2(1.0, 1.0)
         self.localFlip = Vector2(False, False)
 
         self.position = self.localPosition.copy()
-        self.rotation = self.localRotation
         self.scale = self.localScale
         self.flip = self.localFlip
-
-        self.rotationRads = math.radians(self.rotation)
-        self.rotationCos = math.cos(self.rotation)
-        self.rotationSin = math.cos(self.rotation)
 
         self.update()
 
     def update(self):
         self.updateFlip()
         self.updateScale()
-        self.updateRotation()
         self.updatePosition()
 
-    def translate(self, x, y):
+    def getPosition(self) -> Vector2:
+        position = self.localPosition.copy()
+
+        if self.parent is not None:
+            position *= self.parent.getScale()
+            position += self.parent.getPosition()
+
+        return position
+
+    def getScale(self) -> Vector2:
+        scale = self.localScale
+
+        if self.parent is not None:
+            scale *= self.parent.getScale()
+
+        return scale
+
+    def setPosition(self, *args):
+        if len(args) == 1:
+            position = args[0]
+        elif len(args) == 2:
+            position = Vector2(args[0], args[1])
+
+        self.localPosition = position
+
+        if self.parent is not None:
+            parentPosition = self.parent.getPosition()
+            parentScale = self.parent.getScale()
+
+            if parentScale.x == 0.0:
+                return
+            if parentScale.y == 0.0:
+                return
+            
+            self.localPosition -= parentPosition
+
+            self.localPosition.x /= parentScale.x;
+            self.localPosition.y /= parentScale.y;
+
+    def setScale(self, *args):
+        if len(args) == 1:
+            scale = args[0]
+        elif len(args) == 2:
+            scale = Vector2(args[0], args[1])
+        
+        self.localScale = scale
+        if self.parent is not None:
+            parentScale = self.parent.getScale()
+
+            self.localScale.x /= parentScale.x
+            self.localScale.y /= parentScale.y
+
+    def translate(self, x: float, y: float):
         self.localPosition += Vector2(x, y)
 
-    def rotate(self, deg):
-        self.localRotation += deg
-
-    def setScale(self, xScale, yScale):
+    def setLocalScale(self, xScale: float, yScale: float):
         self.localScale = Vector2(xScale, yScale)
 
     def updatePosition(self):
@@ -49,26 +91,7 @@ class Transform:
 
         if self.parent is not None:
             self.position *= self.parent.scale
-
-            cos = self.parent.rotationCos
-            sin = self.parent.rotationSin
-
-            self.position = Vector2(self.position.x * cos - self.position.y * sin,
-                               self.position.y * cos + self.position.x * sin)
-
             self.position += self.parent.position
-
-    def updateRotation(self):
-        self.rotation = self.localRotation
-        parent = self.parent
-
-        while parent is not None:
-            self.rotation += parent.localRotation
-            parent = parent.parent
-
-        self.rotationRads = math.radians(self.rotation)
-        self.rotationCos = math.cos(self.rotationRads)
-        self.rotationSin = math.sin(self.rotationRads)
 
     def updateScale(self):
         self.scale = self.localScale.copy()
