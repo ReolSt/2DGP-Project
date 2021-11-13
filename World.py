ac115162272265da07bc7ce945c823f658350b4b
Emacser <roxman98@naver.com>
2021-11-14 06:08:52 +0900

    [REFACTOR]: Refactor World class.

--- World.py
+++ World.py
@@ -11,48 +11,13 @@ class World(GameObject):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.ui = GameObject(self)
-        self.addChild(self.ui)
-
-        self.uiCamera = self.scene.addCamera(parent=self.ui, layer="UI", order=3)
-        self.ui.addChild(self.uiCamera)
-
-        self.gamePlayUI = GamePlayUI(self.ui)
-        self.ui.addChild(self.gamePlayUI)
-
-        self.score = 0
-        self.coin = 0
-        self.world = 0
-        self.stage = 0
-        self.time = 400
-
-    def update(self, deltaTime):
-        super().update(deltaTime)
-
-        self.gamePlayUI.name = self.name
-        self.gamePlayUI.score = self.score
-        self.gamePlayUI.coin = self.coin
-        self.gamePlayUI.world = self.world
-        self.gamePlayUI.stage = self.stage
-        self.gamePlayUI.time = int(self.time)
-
-        self.time -= deltaTime / 1000
-
-    def loadLevel(self, world, stage, subStage=-1):
+    def load(self, world, stage, subStage=-1):
         if subStage == -1:
             self.level = LevelLoader().load(self, "{}-{}".format(world, stage))
         else:
             self.level = LevelLoader().load(self, "{}-{}-{}".format(world, stage, subStage))
 
         self.addChild(self.level)
-
-        self.name = "MARIO"
-        self.score = 0
-        self.coin = 0
-        self.world = world
-        self.stage = stage
-        self.subStage = subStage
-        self.time = 400
 
         self.backgroundCamera = self.scene.addCamera(parent=self, layer="Background", order=1)
         self.worldCamera = self.scene.addCamera(parent=self, layer="Default", order=2)
@@ -61,7 +26,8 @@ class World(GameObject):
         self.level.addChild(self.backgroundCamera)
 
         self.playerController = PlayerController(self.level, self.worldCamera)
-        self.playerController.player.transform.setLocalScale(1.0, 1.0)
+        self.player = self.playerController.player
+        self.player.transform.setLocalScale(1.0, 1.0)
 
         self.level.addChild(self.playerController)
         self.level.addPlayer(self.playerController.player)
